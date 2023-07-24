d64c1af671ef1a8c1f22f79b4f1394c100af93de
Aadi1309 <91685036+Aadi1309@users.noreply.github.com>
2023-07-25 02:23:16 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+
+
+import Header from "../component/Header";
+import Memes from "../component/Memes";
+
+const Home = () => {
+
+    return (
+
+        <div>
+            <Header />
+            <Memes/>
+        </div>
+    )
+}
+
+export default Home;
